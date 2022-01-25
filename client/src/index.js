2d056291d777b880ee5bbf205b45e042f3743302
Omer David <omerdavid321@gmail.com>
2022-01-25 22:14:55 +0200

    WIP

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './login';
+
+const domContainer = document.querySelector('#ORT');
+ReactDOM.render( React.createElement(Login , null ), domContainer);
